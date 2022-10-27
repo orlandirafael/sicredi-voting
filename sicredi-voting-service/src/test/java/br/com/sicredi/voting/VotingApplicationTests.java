c934441e2bc1c3345027f506248062dec6877a1d
Rafael Mandacaru Orlandi <orlandirafael@gmail.com>
2022-10-27 15:29:37 -0300

    #ajustes
    - Melhoria nos testes

--- sicredi-voting-service/src/test/java/br/com/sicredi/voting/VotingApplicationTests.java
+++ sicredi-voting-service/src/test/java/br/com/sicredi/voting/VotingApplicationTests.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import java.net.URISyntaxException;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.client.RestClientException;
 
@@ -19,14 +20,15 @@ import br.com.sicredi.voting.service.VotoService;
 @SpringBootTest
 class VotingApplicationTests {
 
-  PautaService pautaService;
-  SessaoService sessaoService;
-  VotoService votoService;
+  @Autowired PautaService pautaService;
+  @Autowired SessaoService sessaoService;
+  @Autowired VotoService votoService;
 
   @Test
   void criaNovaPauta() {
     final PautaDTO pautaDto = new PautaDTO();
-    pautaDto.setDescricao("Pauta de teste");
+    pautaDto.setTitulo("Pauta de teste");
+    pautaDto.setDescricao("Pauta para definição dos testes do sistema de votação");
     PautaDTO retorno = pautaService.novaPauta(pautaDto);
     assertNotNull(retorno);
     assertNotNull(retorno.getId());
@@ -34,8 +36,13 @@ class VotingApplicationTests {
 
   @Test
   void abrirSessao() {
+    PautaDTO pautaDto = new PautaDTO();
+    pautaDto.setTitulo("Pauta de teste");
+    pautaDto.setDescricao("Pauta para definição dos testes do sistema de votação");
+    pautaDto = pautaService.novaPauta(pautaDto);
+
     final SessaoDTO sessaoDto = new SessaoDTO();
-    sessaoDto.setIdPauta(1l);
+    sessaoDto.setIdPauta(pautaDto.getId());
     SessaoDTO retorno = sessaoService.abrirSessao(sessaoDto);
     assertNotNull(retorno);
     assertNotNull(retorno.getId());
@@ -43,18 +50,26 @@ class VotingApplicationTests {
 
   @Test
   void realizarVoto() {
+    PautaDTO pautaDto = new PautaDTO();
+    pautaDto.setTitulo("Pauta de teste");
+    pautaDto.setDescricao("Pauta para definição dos testes do sistema de votação");
+    pautaDto = pautaService.novaPauta(pautaDto);
+
+    SessaoDTO sessaoDto = new SessaoDTO();
+    sessaoDto.setIdPauta(pautaDto.getId());
+    sessaoDto = sessaoService.abrirSessao(sessaoDto);
+
     final VotoDTO votoDto = new VotoDTO();
     votoDto.setEscolha("Sim");
+    votoDto.setCpf("04671050064");
     votoDto.setIdAssociado(1l);
-    votoDto.setIdSessao(1l);
+    votoDto.setIdSessao(sessaoDto.getId());
     VotoDTO retorno;
     try {
       retorno = votoService.realizarVoto(votoDto);
       assertNotNull(retorno);
       assertNotNull(retorno.getId());
-    } catch (RestClientException
-        | OpcaoInvalidaVotoException
-        | URISyntaxException e) { // TODO Auto-generated catch block
+    } catch (RestClientException | OpcaoInvalidaVotoException | URISyntaxException e) {
       e.printStackTrace();
     }
   }
